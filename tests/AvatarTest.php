51e9edaef9ee2bd1a22d905db18f2213a1e5ba71
darbaoui imad <darbaoui@gmail.com>
2021-02-03 15:37:35 +0100

    update fix travis-ci

---
+++ tests/AvatarTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Darbaoui\Avatar\Tests;
+
+class AvatarTest extends \Orchestra\Testbench\TestCase
+{
+
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+
+}
